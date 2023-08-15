293b569b5a5b933f918b4c7e4481eac12536597c
Ricardoo <ricardo.tagami17@gmail.com>
2023-08-15 16:39:32 -0300

    Classe Condicional

---
+++ projeto01/src/Conditional.java
@@ -0,0 +1,14 @@
+public class Conditional {
+    public static void main(String[] args) {
+
+        int releaseYear = 2022;
+        boolean includesPlan = true;
+        double movieRating = 8.1;
+
+        if (releaseYear >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme retrô que vale a pena assitir");
+        }
+    }
+}
