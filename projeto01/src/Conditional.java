582e3c1ceceb13fd59345218237ed266e714178e
RicardooFilho <ricardo.tagami17@gmail.com>
2023-08-15 21:31:33 -0300

    Leitura em Java

--- projeto01/src/Conditional.java
+++ projeto01/src/Conditional.java
@@ -4,11 +4,18 @@ public class Conditional {
         int releaseYear = 2022;
         boolean includesPlan = true;
         double movieRating = 8.1;
+        String planType = "plus";
 
         if (releaseYear >= 2022) {
             System.out.println("Lançamento que os clientes estão curtindo!");
         } else {
             System.out.println("Filme retrô que vale a pena assitir");
+        }
+
+        if (includesPlan || planType.equals("plus")) {
+            System.out.println("Filme liberado!");
+        } else {
+            System.out.println("Deve pagar a locação!");
         }
     }
 }
